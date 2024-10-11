98bed9012991a4546c22718cc9f8e15ea1d5f610
Marco Zagaria <marcozagaria@live.com>
2024-10-11 10:30:29 +0200

    creazione db

---
+++ src/main/java/marcozagaria/Application.java
@@ -0,0 +1,14 @@
+package marcozagaria;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("w3_progetto");
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/marcozagaria/ApplicationTest.java
@@ -0,0 +1,13 @@
+package marcozagaria;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
